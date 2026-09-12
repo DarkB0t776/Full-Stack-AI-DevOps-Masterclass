3f3b61214ec264a5aa2f738cab80c5f7945d68c5
darkbot <dima23790@gmail.com>
2026-09-12 19:28:22 +0300

    change text

--- docker/app1-hello/node/app.js
+++ docker/app1-hello/node/app.js
@@ -6,7 +6,7 @@ const HOSTNAME = process.env.HOSTNAME || require('os').hostname();
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Hello from Simple App (Node)",
+    message: "Hello from Simple App (Node1)",
     env: ENV,
     container: HOSTNAME
   });
